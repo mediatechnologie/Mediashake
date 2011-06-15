f49965bff060c776948d8f800698b56c1f00fe91
Farid el Nasire <f.elnasire@vividwebsystems.nl>
2011-06-15 09:13:13 +0200

    Final push before the big deadline of June 20th.

--- includes/config.php
+++ includes/config.php
@@ -1,5 +1,11 @@
 <?php
 
+	define('DB_HOST', 'localhost');
+	define('DB_USER', 'root');
+	define('DB_PASSWORD', '');
+	define('DB_NAME', 'mediashake');
 	
+	define('SITE_NAME', 'Mediashake');
+	define('SITE_URL', 'http://localhost/ms');
 
 ?>
--- index.php
+++ index.php
@@ -1,10 +1,17 @@
 <?php
 
 	session_start();
-
-	require_once('includes/config.php');
 	
-	require_once('classes/site.class.php');
+	require('includes/config.php');
+	require('includes/functions.php');
+	require('jsi.php');
+
+	require('classes/database.class.php');
+	require('classes/site.class.php');
+	require('classes/content.class.php');
+	require('classes/sidebar.class.php');
+	require('classes/work.class.php');
+	require('classes/image.class.php');
 	
 	$Site = new Site;
 	$Site->invoke();
