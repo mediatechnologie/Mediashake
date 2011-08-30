dcd682fa8291b06fd20d6230bc73f846d9114321
duckson <duckson@liberator>
2011-08-30 21:19:19 +0200

    Removed the config file, added an updated sample config.

--- includes/config.sample.php
+++ includes/config.sample.php
@@ -1,11 +1,18 @@
 <?php
-
+	// The database host name, this is usually localhost.
 	define('DB_HOST', 'localhost');
+	
+	// The database user
 	define('DB_USER', 'someone');
+	
+	// The user's password
 	define('DB_PASSWORD', 'sekret');
+	
+	// The name of the database
 	define('DB_NAME', 'mediashake');
 	
+	// The title of your site
 	define('SITE_NAME', 'Mediashake');
+	
+	// The root url of the site, without a trailing slash
 	define('SITE_URL', 'http://mediashake');
-
-?>
