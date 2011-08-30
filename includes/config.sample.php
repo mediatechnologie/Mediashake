186a51e9ac93bd48fccdf67e9811173cdc5acf9a
duckson <duckson@liberator>
2011-08-30 14:21:58 +0200

    Added a default config file.

---
+++ includes/config.sample.php
@@ -0,0 +1,11 @@
+<?php
+
+	define('DB_HOST', 'localhost');
+	define('DB_USER', 'someone');
+	define('DB_PASSWORD', 'sekret');
+	define('DB_NAME', 'mediashake');
+	
+	define('SITE_NAME', 'Mediashake');
+	define('SITE_URL', 'http://mediashake');
+
+?>
