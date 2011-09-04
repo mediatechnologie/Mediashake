ee9d29af3d5aca4bb23660f2efa28239869ee1fb
duckson <duckson@liberator>
2011-09-04 14:18:27 +0200

    Trimmed some whitespace.

--- index.php
+++ index.php
@@ -1,12 +1,9 @@
 <?php
-
 	session_start();
-
+	
 	require('includes/config.php');
 	require('includes/functions.php');
 	require('autoload.php');
 	
-	$Site = new Site;
-	$Site->invoke();
-
-?>
+	$shake = new Site;
+	echo $shake->invoke();
