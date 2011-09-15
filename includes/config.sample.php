ef6736508427b7353e853988f6834eca4d63e710
duckson <duckson@liberator>
2011-09-15 14:08:16 +0200

    Added Smarty constants in the config file.

--- includes/config.sample.php
+++ includes/config.sample.php
@@ -1,18 +1,29 @@
 <?php
-	// The database host name, this is usually localhost.
-	define('DB_HOST', 'localhost');
-	
-	// The database user
-	define('DB_USER', 'someone');
-	
-	// The user's password
-	define('DB_PASSWORD', 'sekret');
-	
-	// The name of the database
-	define('DB_NAME', 'mediashake');
-	
-	// The title of your site
-	define('SITE_NAME', 'Mediashake');
-	
-	// The root url of the site, without a trailing slash
-	define('SITE_URL', 'http://mediashake');
+/**
+ * config.sample.php
+ * configuration file
+ * Defines contants for the app.
+ */
+
+// The database host name, this is usually localhost.
+define('DB_HOST', 'localhost');
+
+// The database user
+define('DB_USER', 'someone');
+
+// The user's password
+define('DB_PASSWORD', 'sekret');
+
+// The name of the database
+define('DB_NAME', 'mediashake');
+
+// The title of your site
+define('SITE_NAME', 'Mediashake');
+
+// The root url of the site, without a trailing slash
+define('SITE_URL', 'http://mediashake');
+
+// Smarty constants
+define('SMARTY_DIR', 'classes/');
+define('SMARTY_SYSPLUGINS_DIR', 'includes/smarty_sysplugins/');
+define('SMARTY_PLUGINS_DIR', 'includes/smarty_plugins/');
